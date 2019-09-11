fd64f65078e465f26bc4b028ff8725e3a7c39e5b
=thinhtpt <=tthinh2128@gmail.com>
2019-09-11 15:02:50 +0700

    add template

---
+++ src/Home/data.source.js
@@ -0,0 +1,660 @@
+import React from 'react';
+export const Nav30DataSource = {
+  wrapper: { className: 'header3 home-page-wrapper jzih1dpqqrg-editor_css' },
+  page: { className: 'home-page' },
+  logo: {
+    className: 'header3-logo jzjgnya1gmn-editor_css',
+    children:
+      'https://gw.alipayobjects.com/mdn/rms_ae7ad9/afts/img/A*-J8NSLj9rbsAAAAAAAAAAABkARQnAQ',
+  },
+  Menu: {
+    className: 'header3-menu',
+    children: [
+      {
+        name: 'item1',
+        className: 'header3-item',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: (
+                <>
+                  <p>订订群</p>
+                </>
+              ),
+              name: 'text',
+            },
+          ],
+        },
+        subItem: [
+          {
+            className: 'item-sub',
+            children: {
+              className: 'item-sub-item jzj8295azrs-editor_css',
+              children: [
+                {
+                  name: 'image0',
+                  className: 'item-image jzj81c9wabh-editor_css',
+                  children:
+                    'https://gw.alipayobjects.com/mdn/rms_ae7ad9/afts/img/A*4_S6ToPfj-QAAAAAAAAAAABkARQnAQ',
+                },
+              ],
+            },
+            name: 'sub~jzj8hceysgj',
+          },
+        ],
+      },
+      {
+        name: 'item2',
+        className: 'header3-item',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: (
+                <>
+                  <p>帮助中心</p>
+                </>
+              ),
+              name: 'text',
+            },
+          ],
+        },
+      },
+    ],
+  },
+  mobileMenu: { className: 'header3-mobile-menu' },
+};
+export const Nav00DataSource = {
+  wrapper: { className: 'header0 home-page-wrapper' },
+  page: { className: 'home-page' },
+  logo: {
+    className: 'header0-logo',
+    children: './logo.png',
+  },
+  Menu: {
+    className: 'header0-menu',
+    children: [
+      {
+        name: 'item0',
+        className: 'header0-item',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: (
+                <>
+                  <p>Home</p>
+                </>
+              ),
+              name: 'text',
+            },
+          ],
+        },
+      },
+      {
+        name: 'item1',
+        className: 'header0-item',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: (
+                <>
+                  <p>Introduction</p>
+                </>
+              ),
+              name: 'text',
+            },
+          ],
+        },
+      },
+      {
+        name: 'item2',
+        className: 'header0-item',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: (
+                <>
+                  <p>About F-Code</p>
+                </>
+              ),
+              name: 'text',
+            },
+          ],
+        },
+      },
+      {
+        name: 'item3',
+        className: 'header0-item',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: (
+                <>
+                  <p>Top Q&amp;A</p>
+                </>
+              ),
+              name: 'text',
+            },
+          ],
+        },
+      },
+    ],
+  },
+  mobileMenu: { className: 'header0-mobile-menu' },
+};
+export const Banner00DataSource = {
+  wrapper: { className: 'banner0' },
+  textWrapper: { className: 'banner0-text-wrapper' },
+  title: {
+    className: 'banner0-title',
+    children: [<p>Welcom K15</p>, <p>to</p>, <p>F-Code</p>],
+  },
+  content: {
+    className: 'banner0-content',
+    children: (
+      <>
+        <p>Apply to F-Code now!</p>
+      </>
+    ),
+  },
+  button: {
+    className: 'banner0-button',
+    children: (
+      <>
+        <p>Sign up with Google</p>
+      </>
+    ),
+  },
+};
+export const Feature30DataSource = {
+  wrapper: { className: 'home-page-wrapper content3-wrapper' },
+  page: { className: 'home-page content3' },
+  OverPack: { playScale: 0.3 },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: (
+          <>
+            <p>Introduction</p>
+          </>
+        ),
+        className: 'title-h1',
+      },
+      {
+        name: 'content',
+        className: 'title-content',
+        children: (
+          <>
+            <p>Explain meaning of F-Code and this application</p>
+          </>
+        ),
+      },
+    ],
+  },
+  block: {
+    className: 'content3-block-wrapper',
+    children: [
+      {
+        name: 'block0',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/ScHBSdwpTkAHZkJ.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: { className: 'content3-title', children: '企业资源管理' },
+          content: {
+            className: 'content3-content',
+            children:
+              '云资源集中编排、弹性伸缩、持续发布和部署，高可用及容灾。',
+          },
+        },
+      },
+      {
+        name: 'block1',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/NKBELAOuuKbofDD.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: { className: 'content3-title', children: '云安全' },
+          content: {
+            className: 'content3-content',
+            children:
+              '按金融企业安全要求打造的完整云上安全体系，全方位保障金融应用及数据安全。',
+          },
+        },
+      },
+      {
+        name: 'block2',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/xMSBjgxBhKfyMWX.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: { className: 'content3-title', children: '云监控' },
+          content: {
+            className: 'content3-content',
+            children:
+              '分布式云环境集中监控，统一资源及应用状态视图，智能分析及故障定位。',
+          },
+        },
+      },
+      {
+        name: 'block3',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/MNdlBNhmDBLuzqp.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: { className: 'content3-title', children: '移动' },
+          content: {
+            className: 'content3-content',
+            children:
+              '一站式移动金融APP开发及全面监控；丰富可用组件，动态发布和故障热修复。',
+          },
+        },
+      },
+      {
+        name: 'block4',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/UsUmoBRyLvkIQeO.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: { className: 'content3-title', children: '分布式中间件' },
+          content: {
+            className: 'content3-content',
+            children:
+              '金融级联机交易处理中间件，大规模分布式计算机，数万笔/秒级并发能力，严格保证交易数据统一性。',
+          },
+        },
+      },
+      {
+        name: 'block5',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/ipwaQLBLflRfUrg.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: { className: 'content3-title', children: '大数据' },
+          content: {
+            className: 'content3-content',
+            children:
+              '一站式、全周期大数据协同工作平台，PB级数据处理、毫秒级数据分析工具。',
+          },
+        },
+      },
+    ],
+  },
+};
+export const Feature60DataSource = {
+  wrapper: { className: 'home-page-wrapper feature6-wrapper' },
+  OverPack: { className: 'home-page feature6', playScale: 0.3 },
+  Carousel: {
+    className: 'feature6-content',
+    dots: false,
+    wrapper: { className: 'feature6-content-wrapper' },
+    titleWrapper: {
+      className: 'feature6-title-wrapper',
+      barWrapper: {
+        className: 'feature6-title-bar-wrapper k08vkm2c1g7-editor_css',
+        children: { className: 'feature6-title-bar k08vkfrw6t-editor_css' },
+      },
+      title: { className: 'feature6-title' },
+    },
+    children: [
+      {
+        title: {
+          className: 'feature6-title-text',
+          children: (
+            <>
+              <p>About F-Code</p>
+            </>
+          ),
+        },
+        className: 'feature6-item',
+        name: 'block0',
+        children: [
+          {
+            md: 8,
+            xs: 24,
+            className: 'feature6-number-wrapper',
+            name: 'child0',
+            number: {
+              className: 'feature6-number',
+              unit: {
+                className: 'feature6-unit k08vkvxgkb-editor_css',
+                children: '万',
+              },
+              children: (
+                150
+              ),
+            },
+            children: {
+              className: 'feature6-text',
+              children: (
+                <>
+                  <span>members</span>
+                </>
+              ),
+            },
+          },
+          {
+            md: 8,
+            xs: 24,
+            className: 'feature6-number-wrapper',
+            name: 'child1',
+            number: {
+              className: 'feature6-number',
+              unit: {
+                className: 'feature6-unit k08vlsban1b-editor_css',
+                children: '亿',
+              },
+              children: (
+                7
+              ),
+            },
+            children: {
+              className: 'feature6-text',
+              children: (
+                <>
+                  <span>years</span>
+                </>
+              ),
+            },
+          },
+          {
+            md: 8,
+            xs: 24,
+            className: 'feature6-number-wrapper',
+            name: 'child2',
+            number: {
+              className: 'feature6-number',
+              unit: {
+                className: 'feature6-unit k08vlwf7nrk-editor_css',
+                children: '亿',
+              },
+              children: (
+                10
+              ),
+            },
+            children: {
+              className: 'feature6-text',
+              children: (
+                <>
+                  <span>contests/year</span>
+                </>
+              ),
+            },
+          },
+        ],
+      },
+      {
+        title: {
+          className: 'feature6-title-text k08vk2qtfqp-editor_css',
+          children: '服务指标',
+        },
+        className: 'feature6-item',
+        name: 'block1',
+        children: [
+          {
+            md: 8,
+            xs: 24,
+            name: 'child0',
+            className: 'feature6-number-wrapper',
+            number: {
+              className: 'feature6-number',
+              unit: { className: 'feature6-unit', children: '万' },
+              children: '116',
+            },
+            children: { className: 'feature6-text', children: '模型数据' },
+          },
+          {
+            md: 8,
+            xs: 24,
+            name: 'child1',
+            className: 'feature6-number-wrapper',
+            number: {
+              className: 'feature6-number',
+              unit: { className: 'feature6-unit', children: '亿' },
+              children: '1.17',
+            },
+            children: { className: 'feature6-text', children: '模型迭代数量' },
+          },
+          {
+            md: 8,
+            xs: 24,
+            name: 'child2',
+            className: 'feature6-number-wrapper',
+            number: {
+              className: 'feature6-number',
+              unit: { className: 'feature6-unit', children: '亿' },
+              children: '2.10',
+            },
+            children: { className: 'feature6-text', children: '训练样本数量' },
+          },
+        ],
+      },
+    ],
+  },
+};
+export const Content90DataSource = {
+  wrapper: { className: 'home-page-wrapper content9-wrapper' },
+  page: { className: 'home-page content9' },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'image',
+        children:
+          'https://gw.alipayobjects.com/zos/rmsportal/PiqyziYmvbgAudYfhuBr.svg',
+        className: 'title-image',
+      },
+      {
+        name: 'title',
+        children: (
+          <>
+            <p>Top Q&amp;A</p>
+          </>
+        ),
+        className: 'title-h1',
+      },
+    ],
+  },
+  block: {
+    className: 'timeline',
+    children: [
+      {
+        name: 'block0',
+        className: 'block-wrapper k08voards38-editor_css',
+        playScale: 0.3,
+        children: {
+          imgWrapper: { className: 'image-wrapper k08vnsi3xs-editor_css' },
+          textWrapper: { className: 'text-wrapper k08vo7ch1wo-editor_css' },
+          img: {
+            className: 'block-img',
+            children:
+              'https://gw.alipayobjects.com/zos/rmsportal/SlFgHDtOTLzccvFrQHLg.png',
+          },
+          icon: {
+            className: 'block-icon',
+            children:
+              'https://gw.alipayobjects.com/zos/rmsportal/qJnGrvjXPxdKETlVSrbe.svg',
+          },
+          name: { className: 'block-name', children: '姓名' },
+          post: { className: 'block-post', children: '公司 职位' },
+          time: { className: 'block-time', children: '09:00 - 10:00' },
+          title: { className: 'block-title', children: '开幕致辞' },
+          content: { className: 'block-content', children: '' },
+        },
+      },
+      {
+        name: 'block1',
+        className: 'block-wrapper',
+        playScale: 0.3,
+        children: {
+          imgWrapper: { className: 'image-wrapper' },
+          textWrapper: { className: 'text-wrapper' },
+          img: {
+            className: 'block-img',
+            children:
+              'https://gw.alipayobjects.com/zos/rmsportal/SlFgHDtOTLzccvFrQHLg.png',
+          },
+          icon: {
+            className: 'block-icon',
+            children:
+              'https://gw.alipayobjects.com/zos/rmsportal/QviGtUPvTFxdhsTUAacr.svg',
+          },
+          name: {
+            className: 'block-name',
+            children: (
+              <>
+                <p>Name</p>
+              </>
+            ),
+          },
+          post: {
+            className: 'block-post',
+            children: (
+              <>
+                <p>Student ID</p>
+              </>
+            ),
+          },
+          time: {
+            className: 'block-time',
+            children: (
+              <>
+                <p>Time</p>
+              </>
+            ),
+          },
+          title: {
+            className: 'block-title',
+            children: (
+              <>
+                <p>Question</p>
+              </>
+            ),
+          },
+          content: {
+            className: 'block-content',
+            children: (
+              <>
+                <p>Answers</p>
+              </>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block2',
+        className: 'block-wrapper',
+        playScale: 0.3,
+        children: {
+          imgWrapper: { className: 'image-wrapper' },
+          textWrapper: { className: 'text-wrapper' },
+          img: {
+            className: 'block-img',
+            children:
+              'https://gw.alipayobjects.com/zos/rmsportal/SlFgHDtOTLzccvFrQHLg.png',
+          },
+          icon: {
+            className: 'block-icon',
+            children:
+              'https://gw.alipayobjects.com/zos/rmsportal/QviGtUPvTFxdhsTUAacr.svg',
+          },
+          name: { className: 'block-name', children: '姓名' },
+          post: { className: 'block-post', children: '公司 职位' },
+          time: { className: 'block-time', children: '09:00 - 10:00' },
+          title: { className: 'block-title', children: '演示标题 - XYZ' },
+          content: {
+            className: 'block-content',
+            children:
+              '经过近 3 年的打磨，在助力中台产品研发效能提升的目标之上，包含设计语言、UI 资产、可视化以及产品体验相关的蚂蚁中台设计体系正在逐步成型。此次分享包含两部分，在介绍蚂蚁设计体系的同时，也会和大家分享我们在设计语言的部分探索。',
+          },
+        },
+      },
+      {
+        name: 'block3',
+        className: 'block-wrapper',
+        playScale: 0.3,
+        children: {
+          imgWrapper: { className: 'image-wrapper' },
+          textWrapper: { className: 'text-wrapper' },
+          img: {
+            className: 'block-img',
+            children:
+              'https://gw.alipayobjects.com/zos/rmsportal/SlFgHDtOTLzccvFrQHLg.png',
+          },
+          icon: {
+            className: 'block-icon',
+            children:
+              'https://gw.alipayobjects.com/zos/rmsportal/agOOBdKEIJlQhfeYhHJc.svg',
+          },
+          name: { className: 'block-name', children: '姓名' },
+          post: { className: 'block-post', children: '公司 职位' },
+          time: { className: 'block-time', children: '09:00 - 10:00' },
+          title: { className: 'block-title', children: '演示标题 - XYZ' },
+          content: {
+            className: 'block-content',
+            children:
+              '经过近 3 年的打磨，在助力中台产品研发效能提升的目标之上，包含设计语言、UI 资产、可视化以及产品体验相关的蚂蚁中台设计体系正在逐步成型。此次分享包含两部分，在介绍蚂蚁设计体系的同时，也会和大家分享我们在设计语言的部分探索。',
+          },
+        },
+      },
+    ],
+  },
+};
+export const Footer00DataSource = {
+  wrapper: { className: 'home-page-wrapper footer0-wrapper' },
+  OverPack: { className: 'home-page footer0', playScale: 0.05 },
+  copyright: {
+    className: 'copyright',
+    children: (
+      <>
+        <span>
+          ©2019 <a href="https://www.facebook.com/fcodefpt/" target="_blank" rel="noopener noreferrer">F-Code</a> All Rights
+          Reserved
+        </span>
+      </>
+    ),
+  },
+};
---
+++ src/Home/index.jsx
@@ -0,0 +1,117 @@
+/* eslint no-undef: 0 */
+/* eslint arrow-parens: 0 */
+import React from "react";
+import { enquireScreen } from "enquire-js";
+
+import Nav3 from "./Nav3";
+import Nav0 from "./Nav0";
+import Banner0 from "./Banner0";
+import Feature3 from "./Feature3";
+import Feature6 from "./Feature6";
+import Content9 from "./Content9";
+import Footer0 from "./Footer0";
+
+import {
+  Nav30DataSource,
+  Nav00DataSource,
+  Banner00DataSource,
+  Feature30DataSource,
+  Feature60DataSource,
+  Content90DataSource,
+  Footer00DataSource
+} from "./data.source";
+import "./less/antMotionStyle.less";
+
+let isMobile;
+enquireScreen(b => {
+  isMobile = b;
+});
+
+const { location } = window;
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMobile,
+      show: !location.port // 如果不是 dva 2.0 请删除
+    };
+  }
+
+  componentDidMount() {
+    // 适配手机屏幕;
+    enquireScreen(b => {
+      this.setState({ isMobile: !!b });
+    });
+    // dva 2.0 样式在组件渲染之后动态加载，导致滚动组件不生效；线上不影响；
+    /* 如果不是 dva 2.0 请删除 start */
+    if (location.port) {
+      // 样式 build 时间在 200-300ms 之间;
+      setTimeout(() => {
+        this.setState({
+          show: true
+        });
+      }, 500);
+    }
+    /* 如果不是 dva 2.0 请删除 end */
+  }
+
+  render() {
+    const children = [
+      <Nav3
+        id="Nav3_0"
+        key="Nav3_0"
+        dataSource={Nav30DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Nav0
+        id="Nav0_0"
+        key="Nav0_0"
+        dataSource={Nav00DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Banner0
+        id="Banner0_0"
+        key="Banner0_0"
+        dataSource={Banner00DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Feature3
+        id="Feature3_0"
+        key="Feature3_0"
+        dataSource={Feature30DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Feature6
+        id="Feature6_0"
+        key="Feature6_0"
+        dataSource={Feature60DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Content9
+        id="Content9_0"
+        key="Content9_0"
+        dataSource={Content90DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Footer0
+        id="Footer0_0"
+        key="Footer0_0"
+        dataSource={Footer00DataSource}
+        isMobile={this.state.isMobile}
+      />
+    ];
+    return (
+      <div
+        className="templates-wrapper"
+        ref={d => {
+          this.dom = d;
+        }}
+      >
+        {/* 如果不是 dva 2.0 替换成 {children} start */}
+        {this.state.show && children}
+        {/* 如果不是 dva 2.0 替换成 {children} end */}
+      </div>
+    );
+  }
+}
