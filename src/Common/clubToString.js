1fd8a43a0fc9057f12fb11373a1a7b516ae14aec
thinhtpt.2000 <tthinh2128@gmail.com>
2019-09-25 06:51:27 +0700

    fix undefined value

--- src/Common/clubToString.js
+++ src/Common/clubToString.js
@@ -1,6 +1,7 @@
 export const clubToString = clubList => {
   let result = "";
   let str = "";
+  if (clubList !== undefined) {
   Object.keys(clubList).forEach(function(item, key) {
     key += 1;
     str += key + ". " + clubList[item] + "; ";
@@ -8,4 +9,8 @@ export const clubToString = clubList => {
   result = str.substring(0, str.length - 2);
   result += ".";
   return result;
+}
+else {
+  return "(trống)";
+}
 };
