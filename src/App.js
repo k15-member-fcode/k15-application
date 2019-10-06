d8987fffd5ebc2059555432c2e0d35e5c91ffa15
thinhtpt.2000 <tthinh2128@gmail.com>
2019-10-06 16:50:21 +0700

    refactor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
-import HomePage from "./components/HomePage";
+import HomePage from "./components/HomePage/HomePage";
 import ApplicationPage from "./components/ApplicationPage";
+import Verify from "./components/Verify";
+import Admin from "./components/Admin";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-const App = (props) => {
+const App = () => {
   return (
     <Router>
       <div className="App">
         <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route excat path="/application" component={ApplicationPage} />
+          <Route exact path="/application" component={ApplicationPage} />
+          <Route path="/verify" component={Verify} />
+          <Route path="/admin" component={Admin} />
+          <Route path="/" component={HomePage} />
         </Switch>
       </div>
     </Router>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,3 @@
-
 export const clubToString = clubList => {
     let result = "";
     let str = "";
